7a4cf6e93afdf8f7ef282f501b640b853ba7d4e3
rmc3408 <rmc3408@protonmail.com>
2020-12-29 16:55:41 -0500

    video 14

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -7,14 +7,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  letters = false;
+  numbers = false;
+  symbols = false;
+  lengths = 0;
+  
 
-
-  onButtonClick() {
-    this.password = 'tested';
-    //console.log('clicked!');
-    //console.log(this.password);
-  }
   // getPassword() {
   //   return this.password;
   // }
+  onButtonClick() {
+    //console.log(`${this.letters},${this.numbers},${this.symbols},${this.lengths}`);
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.letters) {
+      validChars += letters;
+    }
+    if (this.numbers) {
+      validChars += numbers;
+    }
+    if (this.symbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.lengths; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+  
+
+  onChangeLetters() {
+    //console.log('Checkbox letters clicked!');
+    this.letters = !this.letters;
+    //console.log(this.letters);
+  }
+  onChangeNumbers() {
+    //console.log('Checkbox numbers clicked!');
+    this.numbers = !this.numbers;
+    //console.log(this.numbers);
+  }
+  onChangeSymbols() {
+    //console.log('Checkbox symbols clicked!');
+    this.symbols = !this.symbols;
+    //console.log(this.symbols);
+  }
+  onChangeLength(value: string) {
+    const convertedNum = parseInt(value);
+    if (!isNaN(convertedNum)) {
+      this.lengths = convertedNum;
+    }
+
+
+  }
 }
