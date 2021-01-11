66e815635f592778bbb79f95ccd95aa9efab3681
rmc3408 <rmc3408@protonmail.com>
2021-01-10 20:24:57 -0500

    video 47 done

--- typing-challenge/src/app/app.component.ts
+++ typing-challenge/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+const rndSentence = lorem.sentence();
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing-challenge';
+  randomText = rndSentence;
+  text = '';
+  isWinner = false;
+
+  onTyping(evt: any) {
+    //console.log("texting...");
+    //console.log(evt);
+    //console.log(this.text);
+    //this.text = evt.target.value;
+    if (evt === this.randomText) {
+      this.isWinner = true;
+    }
+    else {
+      this.isWinner = false;
+    }
+    this.text = evt;
+  }
+  onCheckColor() {
+    console.log('clor');
+  }
+
 }
